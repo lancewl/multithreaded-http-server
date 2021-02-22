be7525c9ed29d1e237588b04d0b2e4052afe4512
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2021-02-21 22:43:24 -0600

    Add click package for support of CLI interface

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
+import click
 
-def run(server_class=ThreadingHTTPServer, handler_class=SimpleHTTPRequestHandler):
-    server_address = ('', 80)
+@click.command()
+@click.option('--port',
+              default=8888,
+              help='Listening Port')
+def run(port, server_class=ThreadingHTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
