9c5d38f6dde01a00bfdb7c8994fb072119380cf8
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2021-02-23 22:18:56 -0600

    Add customize serving dir to server

--- server/server.py
+++ server/server.py
@@ -1,12 +1,17 @@
 from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
 import click
+import os
 
 @click.command()
 @click.option('--port',
               default=8888,
               help='Listening Port')
 def run(port, server_class=ThreadingHTTPServer, handler_class=SimpleHTTPRequestHandler):
+    target_dir = os.path.join(os.path.dirname(__file__), 'data')
+    os.chdir(target_dir)
+
     server_address = ('', port)
+    print("Server started at localhost:" + str(port))
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
