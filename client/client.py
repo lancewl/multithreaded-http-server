5abc064e26d0f02706b46cd745e645553d9843dc
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2021-02-24 19:57:29 -0600

    Refactor client script

--- client/client.py
+++ client/client.py
@@ -1,57 +1,47 @@
 import requests  
-from bs4 import BeautifulSoup  
-
-# specify the URL of the archive here  
-archive_url = "http://localhost:8888/"
+import hashlib
   
-def get_file_links():  
-      
+def get_file_list(server_url):  
     # create response object  
-    r = requests.get(archive_url)
-      
-    # create beautiful-soup object  
-    soup = BeautifulSoup(r.content,'html5lib')  
-      
-    # find all links on web-page  
-    links = soup.findAll('a')  
-  
-    file_links = [archive_url + link['href'] for link in links if link['href']]  
-  
-    return file_links  
-  
-  
-def download_all_files(file_links):  
-  
-    for link in file_links:  
-        '''iterate through all links in file_links  
-        and download them one by one'''
-          
-        # obtain filename by splitting url and getting  
-        # last string  
-        file_name = link.split('/')[-1]  
-  
-        print( "Downloading file:%s"%file_name)  
-          
-        # create response object  
-        r = requests.get(link, stream = True)  
-          
-        # download started  
+    r = requests.get(server_url)
+    data = r.json()
+    return data
+
+def download_file(file_name, file_link, file_md5):
+    print( "Downloading file:%s"%file_name)  
+    
+    # create response object  
+    r = requests.get(server_url + file_link, stream = True)  
+
+    md5 = hashlib.md5(r.content).hexdigest()
+
+    if md5 == file_md5:
+        print("Checksum successful")
         with open(file_name, 'wb') as f:  
             for chunk in r.iter_content(chunk_size = 1024*1024):  
                 if chunk:  
-                    f.write(chunk)  
-          
-        print( "%s downloaded!\n"%file_name ) 
+                    f.write(chunk)
+    else:
+        print("Checksum failed")
+        
+    print( "%s downloaded!\n"%file_name ) 
+  
+def download_all_files(file_list, server_url):  
+  
+    for file_name, file_info in file_list.items():  
+        if file_info["type"] == "file": # Only download files
+            download_file(file_name, file_info["link"], file_info["md5"])
   
     print ("All files downloaded!") 
     return
   
   
 if __name__ == "__main__":  
+    server_url = "http://localhost:8888/"
   
     # getting all video links  
-    file_links = get_file_links()
-    print(file_links)
+    file_list = get_file_list(server_url)
+    print(file_list)
   
     # download all videos  
-    download_all_files(file_links)  
+    download_all_files(file_list, server_url)
